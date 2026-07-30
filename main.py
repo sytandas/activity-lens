9df8eb7d109cfbbab9ad9bc1a850b7bbf3148262
sytandas <sytandas@gmail.com>
2026-07-30 09:17:02 +0530

    strucuture manipulation

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import math
+from models.ride import Ride
 
-temperature = float(input("Enter temperature (°C): "))
-humidity = float(input("Enter relative humidity (%): "))
+def main():
+    ride = Ride("data/activities/coros-28.fit")
 
-a = 17.27
-b = 237.7
+    ride.load()
+    ride.fetch_weather()
+    ride.calculate_metrics()
+    ride.summary()
 
-# Magnus-Tetens Formula
-alpha = ((a * temperature) / (b + temperature)) + math.log(humidity / 100.0)
-dew_point = (b * alpha) / (a - alpha)
-
-print(f"Dew Point: {dew_point:.2f} °C")
-
-if dew_point < 10:
-    print("Very dry, good for racing")
-elif 10 <= dew_point < 13:
-    print("Comfortable, minimal impact")
-elif 13 <= dew_point < 16:
-    print("Slightly humid, small increase of effort")
-elif 16 <= dew_point < 18:
-    print("Noticeable, pace may slow slightly")
-elif 18 <= dew_point < 21:
-    print("Uncomfortable, noticeable performance loss")
-elif 21 <= dew_point < 24:
-    print("Very humid, significant slowdown")
-else:  # dew_point >= 24
-    print("Oppressive, high heat stress risk")
+if __name__ == "__main__":
+    main()
